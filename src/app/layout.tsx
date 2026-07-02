b7c2f572a4947e53c26eb674ab64cf551b131b6d
kahfice <mkahfi743@gmail.com>
2026-07-02 11:19:19 +0700

    feat: add Workbox core and routing functionality for improved service worker capabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,13 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "VMMS",
   },
+  icons: {
+    apple: "/icon-192x192.png",
+    icon: [
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
